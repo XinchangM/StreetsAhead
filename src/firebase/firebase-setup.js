d536af86c7761cc417d937c01fab730ad917a39a
lilllmeng <lilllmeng@genius.com>
2022-11-23 13:23:27 -0800

    First Iteration: firestore update

--- src/firebase/firebase-setup.js
+++ src/firebase/firebase-setup.js
@@ -22,8 +22,7 @@ const firebaseConfig = {
   appId: appId,
 };
 
-let myApp = initializeApp(firebaseConfig);
-
+const myApp = initializeApp(firebaseConfig);
 export const firestore = getFirestore(myApp);
 export const auth = initializeAuth(myApp, {
   persistence: getReactNativePersistence(AsyncStorage),
--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -9,13 +9,17 @@ import {
   
   import { firestore, auth } from "./firebase-setup";
   
-  export async function saveUser(user) {
+  export async function saveUserInfo(userName) {
     try {
-      await setDoc(doc(firestore, "users", auth.currentUser.uid), user);
+        const docRef = await addDoc(collection(firestore, "users"), {
+        userId:auth.currentUser.uid,
+        userName:userName
+      });
     } catch (err) {
       console.log("save user ", err);
     }
   }
+
   export async function getUser() {
     const docRef = doc(firestore, "users", auth.currentUser.uid);
     const docSnap = await getDoc(docRef);
@@ -27,5 +31,33 @@ import {
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
+    }
+  }
+  export async function writeEventToDB({ startTime,endTime,coordinate,performer,eventName }) {
+    try {
+      const docRef = await addDoc(collection(firestore, "events"), {
+        startTime:startTime,
+        endTime:endTime,
+        coordinate:coordinate,
+        performer:performer,
+        userId:auth.currentUser.uid,
+        eventName:eventName
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  export async function writePostToDB({mediaUri,postTime,linkedEventId,comment}) {
+    try {
+      const docRef = await addDoc(collection(firestore, "posts"), {
+        mediaUri:mediaUri,
+        postTime:postTime,
+        userId:auth.currentUser.uid,
+        linkedEventId :linkedEventId,
+        comment:comment
+      });
+    } catch (err) {
+      console.log(err);
     }
   }
