cf577e30c19c1995430b862d50ce9d358fe99613
lilllmeng <lilllmeng@genius.com>
2022-11-23 15:50:19 -0800

    First Iteration: user dashboard

---
+++ src/userDashboard/ManageEventPage.js
@@ -0,0 +1,33 @@
+import { View, Text,Pressable,StyleSheet } from 'react-native'
+import React from 'react'
+import Button from '../components/Button';
+
+export default function ManageEventPage({route,navigation}) {
+  function checkDetailPressed() {
+    navigation.navigate("EventDetailPage"
+    //{eventId: }
+    );
+  }
+
+  function updatePressed() {
+
+  }
+
+  function deletePressed() {
+ 
+  }
+  return (
+    <View>
+      <Text>ManageEventPage</Text>
+<Button onPress={checkDetailPressed} title={"Check event detail"}/>
+<Button onPress={updatePressed} title={"Update Event Information"}/>
+<Button onPress={deletePressed} title={"Delete event"}/>  
+
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ src/userDashboard/PostHistoryPage.js
@@ -0,0 +1,22 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+import PostList from '../components/PostList';
+
+export default function PostHistoryPage() {
+  return (
+    <View>
+      <Text>PostHistoryPage</Text>
+    <View style={styles.list}>
+<PostList/>
+    </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  list: {
+    // flex: 1,
+    height: "100%",
+  }
+
+});
